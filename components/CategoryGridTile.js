4f6e2251ce04922a5d871dbf5dfddffe50135b2a
aysuakbaba <aysuakbabaa@gmail.com>
2023-05-17 00:02:48 +0300

    style(categorygridtile): ui changes

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,46 +1,45 @@
-import { Pressable, View, Text, StyleSheet } from "react-native"
+import { Pressable, View, Text, StyleSheet } from "react-native";
 
 function CategoryGridTile({ title, color }) {
-    return(
-        <View style={styles.gridItem}>
-            <Pressable style={styles.button} android_ripple={{color: 'violet'}}>
-                <View style={styles.innerContainer}>
-                    <Text style={styles.title}>
-                        {title}
-                    </Text>
-                </View>
-            </Pressable>
-
+  return (
+    <View style={styles.gridItem}>
+      <Pressable style={({ pressed }) => [styles.button, pressed ? styles.buttonPressed : null]} android_ripple={{ color: "#ccc" }}>
+        <View style={[styles.innerContainer, { backgroundColor: color }]}>
+          <Text style={styles.title}>{title}</Text>
         </View>
-    )
-
+      </Pressable>
+    </View>
+  );
 }
 
-
-export default CategoryGridTile
-
+export default CategoryGridTile;
 
 const styles = StyleSheet.create({
-    gridItem: {
-        flex: 1,
-        height: 150,
-        margin:16,
-        borderRadius: 8,
-        elevation: 5,
-        backgroundColor: 'pink'
-    },
-    button:{
-        flex: 1
-    },
-    innerContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 16
-    },
-    title: {
-        fontWeight: 'bold',
-        fontSize: 18,
-    }
+  gridItem: {
+    flex: 1,
+    height: 150,
+    margin: 16,
+    borderRadius: 8,
+    elevation: 4,
+    overflow: "hidden",
+    backgroundColor: '#ccc'
+  },
+  button: {
+    flex: 1,
+  },
+  buttonPressed: {
+    opacity: 0.5,
+  },
+  innerContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 16,
+    borderRadius: 8,
 
-})
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+});
